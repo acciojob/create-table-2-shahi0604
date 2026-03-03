b108aa1acba1ecb8a9e787deb4bb7304f2d4b78a
Shahina <166979082+shahi0604@users.noreply.github.com>
2026-03-03 21:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 function createTable() {
-    //Write your code here
-  
+    
+    let rn = prompt("Input number of rows");
+    let cn = prompt("Input number of columns");
+
+    
+    rn = Number(rn);
+    cn = Number(cn);
+
+   
+    if (isNaN(rn) || isNaN(cn)) {
+        return; 
+    }
+
+    
+    if (rn <= 0 || cn <= 0) {
+        alert("Please enter positive numbers greater than 0.");
+        return;
+    }
+
+    
+    const table = document.getElementById("myTable");
+
+    
+    table.innerHTML = "";
+
+    
+    for (let i = 0; i < rn; i++) {
+        let row = table.insertRow();
+
+        for (let j = 0; j < cn; j++) {
+            let cell = row.insertCell();
+            cell.textContent = `Row-${i} Column-${j}`;
+        }
+    }
 }
